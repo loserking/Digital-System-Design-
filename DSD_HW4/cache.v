cb1855af24cbebc0f11d8335bc5c9a65b0264bff
The King of Loser <r04943133@ntu.edu.tw>
2019-08-11 00:50:43 +0800

    DSD_HW4

---
+++ DSD_HW4/cache.v
@@ -0,0 +1,48 @@
+module cache(
+    clk,
+    proc_reset,
+    proc_read,
+    proc_write,
+    proc_addr,
+    proc_rdata,
+    proc_wdata,
+    proc_stall,
+    mem_read,
+    mem_write,
+    mem_addr,
+    mem_rdata,
+    mem_wdata,
+    mem_ready
+);
+    
+//==== input/output definition ============================
+    input          clk;
+    // processor interface
+    input          proc_reset;
+    input          proc_read, proc_write;
+    input   [29:0] proc_addr;
+    input   [31:0] proc_wdata;
+    output         proc_stall;
+    output  [31:0] proc_rdata;
+    // memory interface
+    input  [127:0] mem_rdata;
+    input          mem_ready;
+    output         mem_read, mem_write;
+    output  [27:0] mem_addr;
+    output [127:0] mem_wdata;
+    
+//==== wire/reg definition ================================
+    
+//==== combinational circuit ==============================
+
+//==== sequential circuit =================================
+always@( posedge clk or posedge proc_reset ) begin
+    if( proc_reset ) begin
+    
+    end
+    else begin
+    
+    end
+end
+
+endmodule
